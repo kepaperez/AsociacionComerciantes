de4ba09175ba5c409e1adabd29a75a9a73a5e996
LanderFI <fontanedalander@gmail.com>
2021-01-13 13:26:15 +0100

    clasess

---
+++ docs/model/usuarioClass.php
@@ -0,0 +1,140 @@
+<?php
+class usuarioClass{
+    public $id;
+    public $nombre;
+    public $apellido;
+    public $dni;
+    public $telefono;
+    public $nombreUsuario;
+    public $contrasenaUsuario;
+    public $direccion;
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getApellido()
+    {
+        return $this->apellido;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDni()
+    {
+        return $this->dni;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTelefono()
+    {
+        return $this->telefono;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNombreUsuario()
+    {
+        return $this->nombreUsuario;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getContrasenaUsuario()
+    {
+        return $this->contrasenaUsuario;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDireccion()
+    {
+        return $this->direccion;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @param mixed $nombre
+     */
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+    }
+
+    /**
+     * @param mixed $apellido
+     */
+    public function setApellido($apellido)
+    {
+        $this->apellido = $apellido;
+    }
+
+    /**
+     * @param mixed $dni
+     */
+    public function setDni($dni)
+    {
+        $this->dni = $dni;
+    }
+
+    /**
+     * @param mixed $telefono
+     */
+    public function setTelefono($telefono)
+    {
+        $this->telefono = $telefono;
+    }
+
+    /**
+     * @param mixed $nombreUsuario
+     */
+    public function setNombreUsuario($nombreUsuario)
+    {
+        $this->nombreUsuario = $nombreUsuario;
+    }
+
+    /**
+     * @param mixed $contrasenaUsuario
+     */
+    public function setContrasenaUsuario($contrasenaUsuario)
+    {
+        $this->contrasenaUsuario = $contrasenaUsuario;
+    }
+
+    /**
+     * @param mixed $direccion
+     */
+    public function setDireccion($direccion)
+    {
+        $this->direccion = $direccion;
+    }
+
+    
+}
---
+++ docs/model/ventasClass.php
@@ -0,0 +1,105 @@
+<?php
+class ventasClass{
+    public $id;
+    public $id_producto;
+    public $id_tienda;
+    public $id_usuario;
+    public $fecha;
+    public $precio;
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId_producto()
+    {
+        return $this->id_producto;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId_tienda()
+    {
+        return $this->id_tienda;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId_usuario()
+    {
+        return $this->id_usuario;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFecha()
+    {
+        return $this->fecha;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPrecio()
+    {
+        return $this->precio;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @param mixed $id_producto
+     */
+    public function setId_producto($id_producto)
+    {
+        $this->id_producto = $id_producto;
+    }
+
+    /**
+     * @param mixed $id_tienda
+     */
+    public function setId_tienda($id_tienda)
+    {
+        $this->id_tienda = $id_tienda;
+    }
+
+    /**
+     * @param mixed $id_usuario
+     */
+    public function setId_usuario($id_usuario)
+    {
+        $this->id_usuario = $id_usuario;
+    }
+
+    /**
+     * @param mixed $fecha
+     */
+    public function setFecha($fecha)
+    {
+        $this->fecha = $fecha;
+    }
+
+    /**
+     * @param mixed $precio
+     */
+    public function setPrecio($precio)
+    {
+        $this->precio = $precio;
+    }
+
+}
