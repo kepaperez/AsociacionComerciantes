9c8c863d686286e2d8818b2328f6bdacc674bbbc
LanderFI <fontanedalander@gmail.com>
2021-01-13 14:02:37 +0100

    tiendasModel

---
+++ docs/js/jsFunctionsTienda.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+    loadTiendas();
+  
+});
+function loadTiendas() {
+
+    var url = "../controller/cGetAllTiendas.php";
+
+    fetch(url, {
+            method: 'GET',
+        })
+        .then(res => res.json()).then(result => {
+            Tiendas = result.list;
+
+        console.log(Tiendas);
+
+        })
+        .catch(error => console.error('Error status:', error));
+};
