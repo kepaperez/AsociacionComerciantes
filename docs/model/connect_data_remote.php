0a98c69f6ee7bc0f5250418c63f71b8a028ca566
kepaperez <kepaperez2001@gmail.com>
2021-01-13 13:14:28 +0100

    classs

---
+++ docs/model/connect_data_remote.php
@@ -0,0 +1,12 @@
+<?php
+
+
+
+class connect_data
+{
+    public $host='localhost';
+    public $userbbdd='grupo3ze';
+    public $passbbdd='Grupo32020';
+    public $ddbbname='grupo3ze_asociacionComerciantes';
+}
+
