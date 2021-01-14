b02f36e7658dc5be6fd455293da1069487dc5e00
kepaperez <kepaperez2001@gmail.com>
2021-01-14 08:51:26 +0100

    tiendas

--- docs/controller/cGetAllTiendas.php
+++ docs/controller/cGetAllTiendas.php
@@ -9,15 +9,10 @@ $response=array();
 $response['list']=$tienda->setList(); 
 
 echo json_encode($response); 
-<<<<<<< HEAD
+
 // var_dump($response);
 
-=======
-
 unset ($tienda);
-<<<<<<< HEAD
 unset ($response); 
-=======
->>>>>>> dd8884b3c54e35e080d84469909e7de34166f348
-unset ($response);
->>>>>>> 8338ce7511fee99f278be03da472cf989d4f43dc
+
+
