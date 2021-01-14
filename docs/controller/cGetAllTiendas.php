b4bac76b6036a4cdac68c1702bc8d589d19237e1
LanderFI <fontanedalander@gmail.com>
2021-01-14 09:29:24 +0100

    responsiveInicio

--- docs/controller/cGetAllTiendas.php
+++ docs/controller/cGetAllTiendas.php
@@ -9,11 +9,6 @@ $response=array();
 $response['list']=$tienda->setList(); 
 
 echo json_encode($response); 
-<<<<<<< HEAD
-// var_dump($response);
-
-=======
 
 unset ($tienda);
->>>>>>> dd8884b3c54e35e080d84469909e7de34166f348
 unset ($response);
