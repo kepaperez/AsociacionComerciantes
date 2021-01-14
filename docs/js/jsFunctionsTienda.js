d95204b6d99a896ad2a5101f9ce26dd4acba78d7
kepaperez <kepaperez2001@gmail.com>
2021-01-14 14:39:17 +0100

    tiendascargar

--- docs/js/jsFunctionsTienda.js
+++ docs/js/jsFunctionsTienda.js
@@ -1,24 +1,21 @@
 var miApp = angular.module('miApp', []);
 miApp.controller('miControlador', function ($scope, $http) {
 
-   
     loadTiendas();
-    $scope.tiendas = [];
+  
 
     function loadTiendas() {
 
-    
         var url = "controller/cGetAllTiendas.php";
-
-        fetch(url, {
-            method: 'GET',
+        $http({
+            method: "POST",
+            url: "controller/cGetAllTiendas.php"
         })
-            .then(res => res.json()).then(result => {
+            .success(function (result) {
+
                 console.log(result.list);
                 $scope.tiendas = result.list;
+            });
+    }
 
-
-            })
-            .catch(error => console.error('Error status:', error));
-    };
 });
