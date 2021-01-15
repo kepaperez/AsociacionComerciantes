3329ecfbf753a48fae177b06e9f031f14f9e7871
endika <dash6657@gmail.com>
2021-01-15 14:24:54 +0100

    footlockerv1

---
+++ docs/controller/cAllFootlocker.php
@@ -0,0 +1,20 @@
+<?php
+
+include_once ("../model/productoTiendaModel.php");
+$data=json_decode(file_get_contents("php://input"),true);
+
+$productoTienda = new productoTiendaModel();
+
+$id=2;
+
+$productoTienda->setId_tienda($id);
+
+$response=array();
+
+$response['list']=$productoTienda-> setList(); 
+
+echo json_encode($response); 
+// var_dump($response);
+unset ($producto);
+
+unset ($response);
--- docs/js/footlocker.js
+++ docs/js/footlocker.js
@@ -1,15 +1,98 @@
+// ====================================ANGULAR=======================
+var miApp = angular.module('miApp', []);
+miApp.controller('miControlador', function ($scope, $http) {
+
+    $http.get("controller/cAllFootlocker.php").then(function (response) {
+
+        $scope.listaTodo = response.data.list;
+
+        console.log($scope.listaTodo)
+        $scope.calculate()
+    })
+
+    $scope.calculate = function () {
+
+           // CARGA DE FONDOS
+
+
+        // ----Trending----
+        $('#columnTrending1').css('background-image', 'url(img/footlocker/' + $scope.listaTodo[7].producto.imagen + '.jpg)');
+        $('#columnTrending2').css('background-image', 'url(img/footlocker/' + $scope.listaTodo[28].producto.imagen + '.jpg)');
+        $('#columnTrending2').css('background-image', 'url(img/footlocker/' + $scope.listaTodo[28].producto.imagen + '.jpg)');
+
+
+        // --------Mas--------
+        $('.columnMas1').css('background-image', 'url(img/footlocker/footlockerHombre.jpg)');
+        $('.columnMas2').css('background-image', 'url(img/footlocker/footlockerMujer.jpg)');
+        $('.columnMas3').css('background-image', 'url(img/footlocker/footlockerNino.jpg)');
+    }
+
+    // declara el array 
+    $scope.listaActual = [];
+
+    $scope.loadProductList = function (tipo) {
+        // Se encarga de recoger los datos dependiendo del genero
+        // se limpia el array 
+
+        //Genera un numero random para que se ordene la seccion Variado
+        $scope.listaActual = [];
+        $scope.random = function () {
+            return 0.5 - Math.random();
+        }
+        for (let index = 0; index < $scope.listaTodo.length; index++) {
+
+            if (tipo == 1) {
+                if ($scope.listaTodo[index].producto.sexo == sex || $scope.listaTodo[index].producto.sexo == 'Unisex') {
+                    $scope.listaActual.push($scope.listaTodo[index].producto)
+                }
+            }
+            else if (tipo == 2) {
+
+                if ($scope.listaTodo[index].producto.sexo == sex) {
+                    $scope.listaActual.push($scope.listaTodo[index].producto)
+                }
+            }
+        }
+        console.log($scope.listaActual);
+    }
+
+    $scope.loadProduct = function (id) {
+
+        // Buscamos el producto que hemos elegido por el id 
+        for (let index = 0; index < $scope.listaTodo.length; index++) {
+
+            if ($scope.listaTodo[index].producto.id == id) {
+                // cuando lo encontramos rellenamos 
+                console.log($scope.listaTodo[index]);
+
+                $('.boxImg').css('background-image', 'url(img/footlocker/' + $scope.listaTodo[index].producto.imagen + '.jpg)');
+                $('.nombre').html($scope.listaTodo[index].producto.nombre)
+                $('.genero').html($scope.listaTodo[index].producto.sexo)
+                $('.descripcion').html($scope.listaTodo[index].producto.descripcion)
+                $('.precio').html($scope.listaTodo[index].precio + '€')
+            }
+
+        }
+
+    }
+})
+
+// ===================================JQUERY================================
 document.addEventListener('click', function (evt) {
     if (evt.target.className === 'comprarBoton') {
-        ver('divComprar');
+        ver('divComprar', 'loadProducto', evt.target.value);
     }
     if (evt.target.className === 'comprarBotonArt') {
-        ver('divComprar');
+        ver('divComprar', 'loadProducto', evt.target.value);
     }
 }, false);
 
-function ver(where, titulo, id) {
-    console.log(id);
+var sex = '';
 
+function ver(where, titulo, id) {
+
+    console.log(where);
+    
     $("#btn1").removeClass("borde");
     $("#btn2").removeClass("borde");
     $("#btn3").removeClass("borde");
@@ -24,12 +107,44 @@ function ver(where, titulo, id) {
     $('#' + where).show();
     $('#titulo').html(titulo);
 
+    if (titulo == 'loadProducto') {
 
+        angular.element(document.getElementById('miControlador')).scope().loadProduct(id)
+    }
+
+    if (where == 'divTodo') {
+
+        // angular.element(document.getElementById('miControlador')).scope().getAll();
+    }
+
+    if (id == 'btn2') {
+        // seccion mujeres
+        sex = 'Mujer';
+        angular.element(document.getElementById('miControlador')).scope().loadProductList('1')
+    }
+
+    if (id == 'btn3') {
+        // seccion hombres
+        sex = 'Hombre';
+        angular.element(document.getElementById('miControlador')).scope().loadProductList('1')
+    }
+
+    if (id == 'btn4') {
+        // seccion ninos
+        sex = 'Nino';
+        angular.element(document.getElementById('miControlador')).scope().loadProductList('2')
+    }
+
+    if (id == 'btn4') {
+        // seccion ninos
+        sex = 'Nino';
+        angular.element(document.getElementById('miControlador')).scope().loadProductList('2')
+    }
 }
 
 function move(donde, action) {
-    
-    var btn = $(event.target);    
+
+    var btn = $(event.target);
     btn.prop('disabled', true);
     setTimeout(function () {
         btn.prop('disabled', false);
@@ -44,17 +159,13 @@ function move(donde, action) {
     var newMargen = Math.trunc(margen);
 
     if (action == 'izquierda') {
-
-        console.log('<');
         if (newMargen != 0 || newMargen != -0) {
             $('.' + donde).animate({ 'marginLeft': "+=32vw" });
 
         }
 
     } else if (action = 'derecha') {
-
-        console.log('>');
-        if (newMargen <= -63) {
+        if (newMargen <= -224) {
 
 
         } else {
@@ -63,5 +174,82 @@ function move(donde, action) {
 
 
     }
-    console.log(newMargen);
+  
 }
+
+
+
+
+
+
+
+
+
+// document.addEventListener('click', function (evt) {
+//     if (evt.target.className === 'comprarBoton') {
+//         ver('divComprar');
+//     }
+//     if (evt.target.className === 'comprarBotonArt') {
+//         ver('divComprar');
+//     }
+// }, false);
+
+// function ver(where, titulo, id) {
+//     console.log(id);
+
+//     $("#btn1").removeClass("borde");
+//     $("#btn2").removeClass("borde");
+//     $("#btn3").removeClass("borde");
+//     $("#btn4").removeClass("borde");
+
+//     $('#' + id).addClass("borde");
+
+//     $('#divComprar').hide();
+//     $('#divPrincipal').hide();
+//     $('#divSecundario').hide();
+//     $('#divTodo').hide();
+//     $('#' + where).show();
+//     $('#titulo').html(titulo);
+
+
+// }
+
+// function move(donde, action) {
+    
+//     var btn = $(event.target);    
+//     btn.prop('disabled', true);
+//     setTimeout(function () {
+//         btn.prop('disabled', false);
+//     }, 900);
+
+//     var p = document.getElementById(donde);
+
+//     var style = p.style;
+
+//     var margen = (style.marginLeft).replaceAll("vw", "");
+
+//     var newMargen = Math.trunc(margen);
+
+//     if (action == 'izquierda') {
+
+//         console.log('<');
+//         if (newMargen != 0 || newMargen != -0) {
+//             $('.' + donde).animate({ 'marginLeft': "+=32vw" });
+
+//         }
+
+//     } else if (action = 'derecha') {
+
+//         console.log('>');
+//         if (newMargen <= -63) {
+
+
+//         } else {
+//             $('.' + donde).animate({ 'marginLeft': "-=32vw" });
+//         }
+
+
+//     }
+//     console.log(newMargen);
+// }
+
