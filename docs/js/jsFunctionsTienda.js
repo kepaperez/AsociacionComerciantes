2ce27f2a1d8eabbb20f904fdd3b671b00358d868
kepaperez <kepaperez2001@gmail.com>
2021-01-18 09:28:50 +0100

    enlace

--- docs/js/jsFunctionsTienda.js
+++ docs/js/jsFunctionsTienda.js
@@ -17,10 +17,40 @@ miApp.controller('miControlador', function ($scope, $http) {
                 $scope.tiendas = result.list;
             });
     }
-    function tipo(deviceValue) {
 
-        tipo = document.getElementById('comboMenus').value;
-        console.log(deviceValue);
-    }
+
+    $scope.abrirSociedad = function (item) {
+        console.log(item);
+        switch (item.tipo) {
+            case 'Moda':
+                console.log('moda');
+                if (item.id == 2) {
+                    window.location.href = "footlocker.html";
+                } else {
+                    window.location.href = "nike.html";
+                }
+
+                break;
+            case 'Alimentacion':
+                console.log('alimetacion');
+                id= item.id;
+                var id = $(this).attr("id");
+                localStorage.setItem('idTienda', idTienda);
+
+                window.location.href = "alimentacion.html";
+                break;
+            case 'Cosmetica':
+                console.log('cosmetica');
+                var idTienda= item.id;
+                // var idTienda = $(this).attr("idTienda");
+                localStorage.setItem('idTienda', idTienda);
+
+                window.location.href = "cosmetica.html";
+                break;
+
+            default:
+                console.log('Lo lamentamos, por el momento no disponemos ');
+        }
+    };
 
 });
