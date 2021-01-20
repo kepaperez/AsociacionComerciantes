0392e058738a52bc5274b6c241905c05c9b17a1a
kepaperez <kepaperez2001@gmail.com>
2021-01-20 09:49:15 +0100

    cosmetica

--- docs/js/cosmetica.js
+++ docs/js/cosmetica.js
@@ -2,27 +2,43 @@ var miApp = angular.module('miApp', []);
 miApp.controller('miControlador', function ($scope, $http) {
 
     let id = localStorage.getItem('idTienda');
-    loadProductos(id);
+    $scope.data = ({id: id});
+       
+    $http.post("controller/cAllProductos.php", $scope.data).then(function (response) {
 
-    $scope.productos=[];
-    function loadProductos(id) {
+        $scope.productos = response.data.list;
+       
+        console.log($scope.productos);
 
-        var url = "controller/cAllProductos.php";
-        var data = { 'id': id };
+      
 
-        fetch(url, {
-            method: 'POST', // or 'POST'
-            body: JSON.stringify(data), // data can be `string` or {object}!
-            headers: { 'Content-Type': 'application/json' } //input data
+    })
 
-        })
-            .then(res => res.json()).then(result => {
+
+
+    // let id = localStorage.getItem('idTienda');
+    // loadProductos(id);
+
+    // $scope.productos=[];
+    // function loadProductos(id) {
+
+    //     var url = "controller/cAllProductos.php";
+    //     var data = { 'id': id };
+
+    //     fetch(url, {
+    //         method: 'POST', // or 'POST'
+    //         body: JSON.stringify(data), // data can be `string` or {object}!
+    //         headers: { 'Content-Type': 'application/json' } //input data
+
+    //     })
+    //         .then(res => res.json()).then(result => {
                 
-                $scope.productos = result.list;
-                console.log($scope.productos);
+    //             $scope.productos = result.list;
+    //             console.log($scope.productos);
                 
 
-            })
-            .catch(error => console.error('Error status:', error));
-    };
+    //         })
+    //         .catch(error => console.error('Error status:', error));
+    // };
+ 
 });
