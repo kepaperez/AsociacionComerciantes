e5d266c62c6c43cd23c350f755d4aaf219d34743
kepaperez <kepaperez2001@gmail.com>
2021-01-20 13:37:58 +0100

    try

--- docs/js/cosmetica.js
+++ docs/js/cosmetica.js
@@ -2,26 +2,20 @@ var miApp = angular.module('miApp', []);
 miApp.controller('miControlador', function ($scope, $http) {
 
     let id = localStorage.getItem('idTienda');
-    $scope.data = ({id: id});
-       
+    $scope.data = ({ id: id });
+
     $http.post("controller/cAllProductos.php", $scope.data).then(function (response) {
 
         $scope.productos = response.data.list;
-       
+
         console.log($scope.productos);
-
-      
-
     })
-    $scope.data = ({id: id});
+
+    $scope.data = ({ id: id });
     $http.post("controller/cTiendaInfo.php", $scope.data).then(function (response) {
 
-        $scope.tiendaInfo = response.data.list;
-       
-        console.log($scope.tiendaInfo);
+        $scope.tiendaInfo = response.data.tienda;
 
-      
-
+        console.log(response.data.tienda);
     })
-
 });
