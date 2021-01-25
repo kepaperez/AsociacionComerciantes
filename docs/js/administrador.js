cc8213ee566eca365801ceccd9890250d8edfadc
kepaperez <kepaperez2001@gmail.com>
2021-01-25 10:32:34 +0100

    admin

--- docs/js/administrador.js
+++ docs/js/administrador.js
@@ -1,5 +1,21 @@
 var miApp = angular.module('miApp', []);
 miApp.controller('miControlador', function ($scope, $http) {
 
-   
+    loadTiendas();
+
+
+    function loadTiendas() {
+
+        var url = "controller/cGetAllTiendas.php";
+        $http({
+            method: "POST",
+            url: "controller/cGetAllTiendas.php"
+        })
+            .success(function (result) {
+
+                console.log(result.list);
+                $scope.tiendas = result.list;
+            });
+    }
+    
 });
