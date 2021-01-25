b9c7056af3ba905b99d3b454f31257cccce9907a
LanderFI <fontanedalander@gmail.com>
2021-01-25 12:44:42 +0100

    update

--- docs/js/administrador.js
+++ docs/js/administrador.js
@@ -45,5 +45,34 @@ miApp.controller('miControlador', function ($scope, $http) {
     
             .catch(error => console.error('Error status:', error));
     }
+     //El boton
+    // $("#enviar").click(function () {
+
+    //     alert("El usuario se va a actualizar");
+
+    //     updateUsuario();
+    // });
+    function updateUsuario(){
+        var nombre = $("#nombre").val()
+        var apellido = $("#apellido").val()
+        var dni = $("#dni").val()
+        var admin = $("#admin").val()
+        var telefono = $("#telefono").val()
+        var nombreUsuario = $("#nombreUsuario").val()
+        var contrasenaUsuario = $("#contrasenaUsuario").val()
+        var direccion = $("#direccion").val()
+        var saldo = $("#saldo").val()
+
+
+        var url = "../controller/cUpdateUser.php";
+        var data = { 'nombre': nombre, 'apellido':apellido,'dni': dni, 'admin':admin,  'telefono': telefono, 'nombreUsuario': nombreUsuario, 'contrasenaUsuario': contrasenaUsuario, 'direccion': direccion, 'saldo': saldo};
+
+        fetch(url, {
+            method: 'POST', // or 'POST'
+            body: JSON.stringify(data), // data can be `string` or {object}!
+            headers: { 'Content-Type': 'application/json' } //input data
+        })
     
+            .catch(error => console.error('Error status:', error));
+    }
 });
