8fc51bbfac1ef7a85ecc6812e4cce04020e12293
LanderFI <fontanedalander@gmail.com>
2021-01-25 12:34:27 +0100

    UpdateTienda

--- docs/js/administrador.js
+++ docs/js/administrador.js
@@ -4,6 +4,7 @@ miApp.controller('miControlador', function ($scope, $http) {
     loadTiendas();
 
 
+
     function loadTiendas() {
 
         var url = "controller/cGetAllTiendas.php";
@@ -16,6 +17,33 @@ miApp.controller('miControlador', function ($scope, $http) {
                 console.log(result.list);
                 $scope.tiendas = result.list;
             });
+    }
+    //El boton
+    // $("#enviar").click(function () {
+
+    //     alert("La tienda se va a actualizar");
+
+    //     updateTienda();
+    // });
+    function updateTienda(){
+        var nombre = $("#nombre").val()
+        var direccion = $("#direccion").val()
+        var horario = $("#horario").val()
+        var imagen = $("#imagen").val()
+        var tipo = $("#tipo").val()
+        var imgBanner = $("#imgBanner").val()
+
+
+        var url = "../controller/cUpdateUser.php";
+        var data = { 'nombre': nombre, 'direccion': direccion, 'horario': horario, 'imagen': imagen, 'tipo': tipo, 'imgBanner': imgBanner};
+
+        fetch(url, {
+            method: 'POST', // or 'POST'
+            body: JSON.stringify(data), // data can be `string` or {object}!
+            headers: { 'Content-Type': 'application/json' } //input data
+        })
+    
+            .catch(error => console.error('Error status:', error));
     }
     
 });
